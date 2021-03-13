79597b6f4cfbb118f65487eea7822dee54f466b4
JanoDetzel <jano.detzel@gmail.com>
2021-03-13 11:28:37 +0100

    Task #004, #005 fertiggestellt.

---
+++ src/main/java/dhbw/fowler2/theatre/PerformanceCalculator.java
@@ -0,0 +1,29 @@
+package dhbw.fowler2.theatre;
+
+public class PerformanceCalculator {
+
+    public int calculateAmount(Play play, Performance performance) {
+        var calculatedAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                calculatedAmount = 40000;
+                if (performance.audience > 30) {
+                    calculatedAmount += 1000 * (performance.audience - 30);
+                }
+                break;
+            case "comedy":
+                calculatedAmount = 30000;
+                if (performance.audience > 20) {
+                    calculatedAmount += 10000 + 500 * (performance.audience - 20);
+                }
+                calculatedAmount += 300 * performance.audience;
+                break;
+            default:
+                throw new Error("unknown type: ${play.type}");
+        }
+        return calculatedAmount;
+
+
+    }
+}
--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -11,12 +11,14 @@ public class StatementPrinter {
         var volumeCredits = 0;
         var result = String.format("Statement for %s\n", invoice.customer);
 
+        PerformanceCalculator performanceCalculator = new PerformanceCalculator();
+
         NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
         for (var perf : invoice.performances) {
             var play = plays.get(perf.playID);
 
-            int thisAmount = calculateAmount(play, perf);
+            int thisAmount = performanceCalculator.calculateAmount(play, perf);
 
             // add volume credits
             volumeCredits += Math.max(perf.audience - 30, 0);
@@ -30,29 +32,6 @@ public class StatementPrinter {
         result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
-    }
-
-    private int calculateAmount(Play play, Performance performance) {
-        var calculatedAmount = 0;
-
-        switch (play.type) {
-            case "tragedy":
-                calculatedAmount = 40000;
-                if (performance.audience > 30) {
-                    calculatedAmount += 1000 * (performance.audience - 30);
-                }
-                break;
-            case "comedy":
-                calculatedAmount = 30000;
-                if (performance.audience > 20) {
-                    calculatedAmount += 10000 + 500 * (performance.audience - 20);
-                }
-                calculatedAmount += 300 * performance.audience;
-                break;
-            default:
-                throw new Error("unknown type: ${play.type}");
-        }
-        return calculatedAmount;
     }
 
 }
