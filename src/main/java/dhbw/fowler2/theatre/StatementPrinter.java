4a14272624f07d446f78a86a11351c2b547aae25
JanoDetzel <jano.detzel@gmail.com>
2021-03-13 12:55:51 +0100

    Task #008, #009 fertiggestellt.

--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -14,16 +14,15 @@ public class StatementPrinter {
         PerformanceCalculator performanceCalculator = new PerformanceCalculator();
 
         for (var performance : invoice.performances) {
-            // add volume credits
-            volumeCredits += Math.max(performance.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy" == getPlay(plays, performance).type) volumeCredits += Math.floor(performance.audience / 5);
+            volumeCredits += calculateVolumeCredits(performance, plays);
 
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", getPlay(plays, performance).name, NumberFormat.getCurrencyInstance(Locale.US).format(performanceCalculator.calculateAmount(getPlay(plays, performance), performance) / 100), performance.audience);
+            result += String.format("  %s: %s (%s seats)\n", getPlay(plays, performance).name, getNumberFormat(Locale.US)
+                    .format(performanceCalculator
+                            .calculateAmount(getPlay(plays, performance), performance) / 100), performance.audience);
             totalAmount += performanceCalculator.calculateAmount(getPlay(plays, performance), performance);
         }
-        result += String.format("Amount owed is %s\n", NumberFormat.getCurrencyInstance(Locale.US).format(totalAmount / 100));
+        result += String.format("Amount owed is %s\n", getNumberFormat(Locale.US).format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
     }
@@ -32,4 +31,15 @@ public class StatementPrinter {
         return plays.get(performance.playID);
     }
 
+    public NumberFormat getNumberFormat(Locale locale){
+        return NumberFormat.getCurrencyInstance(locale);
+    }
+
+    public int calculateVolumeCredits(Performance performance, Map<String, Play> plays) {
+        int volumeCredits = 0;
+        volumeCredits += Math.max(performance.audience - 30, 0);
+        if ("comedy" == getPlay(plays, performance).type) volumeCredits += Math.floor(performance.audience / 5);
+
+        return volumeCredits;
+    }
 }
