ccfc4073a3753f3b778b0297cb090771f44c54e0
DanielFiller30 <62553667+DanielFiller30@users.noreply.github.com>
2021-03-13 13:08:30 +0100

    Task #010, #011 fertiggestellt.

--- src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
+++ src/main/java/dhbw/fowler2/theatre/StatementPrinter.java
@@ -6,28 +6,27 @@ import java.util.Map;
 
 public class StatementPrinter {
 
+    private Invoice invoice = null;
+    private Map<String, Play> plays = null;
+    PerformanceCalculator performanceCalculator = new PerformanceCalculator();
+
     public String print(Invoice invoice, Map<String, Play> plays) {
-        var totalAmount = 0;
-        var volumeCredits = 0;
+        this.invoice = invoice;
+        this.plays = plays;
         var result = String.format("Statement for %s\n", invoice.customer);
 
-        PerformanceCalculator performanceCalculator = new PerformanceCalculator();
-
         for (var performance : invoice.performances) {
-            volumeCredits += calculateVolumeCredits(performance, plays);
-
             // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", getPlay(plays, performance).name, getNumberFormat(Locale.US)
+            result += String.format("  %s: %s (%s seats)\n", getPlay(performance).name, getNumberFormat(Locale.US)
                     .format(performanceCalculator
-                            .calculateAmount(getPlay(plays, performance), performance) / 100), performance.audience);
-            totalAmount += performanceCalculator.calculateAmount(getPlay(plays, performance), performance);
+                            .calculateAmount(getPlay(performance), performance) / 100), performance.audience);
         }
-        result += String.format("Amount owed is %s\n", getNumberFormat(Locale.US).format(totalAmount / 100));
-        result += String.format("You earned %s credits\n", volumeCredits);
+        result += String.format("Amount owed is %s\n", getNumberFormat(Locale.US).format(getTotalAmount() / 100));
+        result += String.format("You earned %s credits\n", getTotalVolumeCredits());
         return result;
     }
 
-    public Play getPlay(Map<String, Play> plays, Performance performance) {
+    public Play getPlay(Performance performance) {
         return plays.get(performance.playID);
     }
 
@@ -35,11 +34,31 @@ public class StatementPrinter {
         return NumberFormat.getCurrencyInstance(locale);
     }
 
-    public int calculateVolumeCredits(Performance performance, Map<String, Play> plays) {
+    public int calculateVolumeCredits(Performance performance) {
         int volumeCredits = 0;
         volumeCredits += Math.max(performance.audience - 30, 0);
-        if ("comedy" == getPlay(plays, performance).type) volumeCredits += Math.floor(performance.audience / 5);
+        if ("comedy" == getPlay(performance).type) volumeCredits += Math.floor(performance.audience / 5);
 
         return volumeCredits;
+    }
+
+    private int getTotalAmount() {
+        int result = 0;
+
+        for (var performance : invoice.performances) {
+            result += performanceCalculator.calculateAmount(getPlay(performance), performance);
+        }
+
+        return result;
+    }
+
+    private int getTotalVolumeCredits() {
+        int result = 0;
+
+        for (var performance : invoice.performances) {
+            result += calculateVolumeCredits(performance);
+        }
+
+        return result;
     }
 }
